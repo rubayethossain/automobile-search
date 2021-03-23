88d0d94ce6fbda8ae3783505d13df2f4ba09ec37
Md. Rubayet Hossain <rubayet33@gmail.com>
2021-03-24 00:13:55 +0600

    Setup Bulma UI library

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Welcome</div>;
+  return (
+    <div className="App">
+      <h1 className="is-size-1 has-text-centered has-background-link-dark has-text-white">
+        Automobile Search
+      </h1>
+    </div>
+  );
 }
 
 export default App;
