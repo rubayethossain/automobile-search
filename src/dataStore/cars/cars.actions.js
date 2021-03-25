0410544e82c1ba9392e5e508676a3b583b0d9617
Md. Rubayet Hossain <rubayet33@gmail.com>
2021-03-25 19:50:24 +0600

    Create actions

--- src/dataStore/cars/cars.actions.js
+++ src/dataStore/cars/cars.actions.js
@@ -1,7 +1,50 @@
-import { GET_CARS } from "./cars.type";
+import {
+  ADD_CAR,
+  ADD_COMMENT,
+  CAR_DETAILS,
+  DELETE_CAR,
+  GET_CARS,
+  UPDATE_CAR,
+} from "./cars.type";
 
 export const getAllCars = () => {
   return {
     type: GET_CARS,
+  };
+};
+
+export const addCar = (carDetails) => {
+  return {
+    type: ADD_CAR,
+    payload: carDetails,
+  };
+};
+
+export const updateCar = (carId, updateData) => {
+  return {
+    type: UPDATE_CAR,
+    payload: updateData,
+    id: carId,
+  };
+};
+
+export const deleteCar = (carId) => {
+  return {
+    type: DELETE_CAR,
+    id: carId,
+  };
+};
+
+export const carDetails = (carId) => {
+  return {
+    type: CAR_DETAILS,
+    id: carId,
+  };
+};
+
+export const addComment = (carId, comment) => {
+  return {
+    type: ADD_COMMENT,
+    payload: comment,
   };
 };
