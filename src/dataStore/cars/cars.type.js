0755a1e72441a1a9de05d2c1314fbc3ee408c1c1
Md. Rubayet Hossain <rubayet33@gmail.com>
2021-03-25 19:22:19 +0600

    Create action types based on different cases

--- src/dataStore/cars/cars.type.js
+++ src/dataStore/cars/cars.type.js
@@ -1,1 +1,6 @@
 export const GET_CARS = "GET_CARS";
+export const ADD_CAR = "ADD_CAR";
+export const UPDATE_CAR = "UPDATE_CAR";
+export const DELETE_CAR = "DELETE_CAR";
+export const CAR_DETAILS = "CAR_DETAILS";
+export const ADD_COMMENT = "ADD_COMMENT";
