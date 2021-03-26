d365621adb0fecdf66e965c49ef6caf07105aaa1
Md. Rubayet Hossain <rubayet.dev@gmail.com>
2021-03-26 11:53:31 +0600

    Fetch currency rate and provide via context

--- src/contexts/CurrencyProvider.js
+++ src/contexts/CurrencyProvider.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CurrencyContext from "./CurrencyContext";
 
 function CurrencyProvider({ children }) {
+  const [currency, setCurrency] = useState("USD");
+  const [rate, setRate] = useState(1);
+  const [currencyRates, setCurrencyRates] = useState({});
+
+  useEffect(() => {
+    const fetchCurrencyRates = async () => {
+      try {
+        const req = await fetch(
+          "https://api.exchangeratesapi.io/latest?base=USD&symbols=EUR,GBP,JPY"
+        );
+        const res = await req.json();
+
+        setCurrencyRates(res.rates);
+      } catch (error) {}
+    };
+
+    fetchCurrencyRates();
+  }, []);
+
+  useEffect(() => {
+    if (currency === "USD") {
+      setRate(1);
+    } else {
+      setRate(currencyRates[currency]);
+    }
+  }, [currency, currencyRates]);
+
   return (
-    <CurrencyContext.Provider value={{}}>{children}</CurrencyContext.Provider>
+    <CurrencyContext.Provider value={{ rate, setCurrency }}>
+      {children}
+    </CurrencyContext.Provider>
   );
 }
 
