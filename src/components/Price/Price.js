6dd4fade47422f5d2f382deb47a89b4869bbbccf
Md. Rubayet Hossain <rubayet.dev@gmail.com>
2021-03-26 12:00:22 +0600

    Show currency beside amount

--- src/components/Price/Price.js
+++ src/components/Price/Price.js
@@ -2,8 +2,13 @@ import { CurrencyContext } from "contexts";
 import React, { useContext } from "react";
 
 function Price({ amount, ...props }) {
-  const { rate } = useContext(CurrencyContext);
-  return <h4 {...props}>{parseInt(amount, 10) * rate}</h4>;
+  const { rate, currency } = useContext(CurrencyContext);
+  return (
+    <h4 {...props}>
+      <span className="mr-1">{currency}</span>
+      {parseInt(amount, 10) * rate}
+    </h4>
+  );
 }
 
 export default Price;
